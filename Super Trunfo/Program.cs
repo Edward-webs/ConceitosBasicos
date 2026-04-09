a9a2779355cb3614151fefe87959360a33bdd625
Edward-webs <edulyra.silva@gmail.com>
2026-04-08 23:42:15 -0300

    supertrunfo refeito

--- Super Trunfo/Program.cs
+++ Super Trunfo/Program.cs
@@ -1,8 +1,8 @@
-﻿#region Dudu
-using System;
-using System.Collections.Generic;
-public class Card
+﻿using System;
+
+public class Carta
 {
+    // criação da classe carta com atributos e métodos
     private string _code;
     private string _name;
     private int _hp;
@@ -10,6 +10,7 @@ public class Card
     private int _speed;
     private int _strength;
     private int _resistance;
+
     public string Code
     {
         get { return _code; }
@@ -45,7 +46,8 @@ public class Card
         get { return _resistance; }
         set { _resistance = value; }
     }
-    public Card(string code, string name, int hp, int power, int speed, int strength, int resistance)
+
+    public Carta(string code, string name, int hp, int power, int speed, int strength, int resistance)
     {
         _code = code;
         _name = name;
@@ -55,180 +57,246 @@ public class Card
         _strength = strength;
         _resistance = resistance;
     }
+
     public void displayCard()
     {
-        Console.WriteLine("-----Carta-----:");
-        Console.WriteLine($"Código: {_code}");
-        Console.WriteLine($"Nome: {_name}");
-        Console.WriteLine($"HP: {_hp}");
-        Console.WriteLine($"Poder: {_power}");
-        Console.WriteLine($"Velocidade: {_speed}");
-        Console.WriteLine($"Força: {_strength}");
-        Console.WriteLine($"Resistência: {_resistance}");
-        Console.WriteLine("---------------");
+        // função para exibir a carta e seus atributos
+        Console.WriteLine("\n------- SUA CARTA -------");
+        Console.WriteLine($"[{_code}] {_name}");
+        Console.WriteLine($"A. HP: {_hp}");
+        Console.WriteLine($"B. Poder: {_power}");
+        Console.WriteLine($"C. Velocidade: {_speed}");
+        Console.WriteLine($"D. Força: {_strength}");
+        Console.WriteLine($"E. Resistência: {_resistance}");
+        Console.WriteLine("------------------------");
     }
 }
-public class Deck
+
+public class Baralho
 {
-    private List<Card> _cards = new List<Card>();
-    public Deck()
+    private Carta[] _cards = new Carta[32];
+
+    public Baralho()
     {
-        createCard();
+        // função que cria e adiciona cartas ao array Carta[]"
+        _cards[0] = new Carta("A1", "Guerreiro", 100, 85, 40, 80, 90);
+        _cards[1] = new Carta("A2", "Mago", 80, 90, 60, 70, 60);
+        _cards[2] = new Carta("A3", "Arqueiro", 90, 75, 80, 65, 70);
+        _cards[3] = new Carta("A4", "Ladino", 70, 80, 90, 60, 50);
+        _cards[4] = new Carta("A5", "Paladino", 110, 80, 50, 85, 95);
+        _cards[5] = new Carta("A6", "Bárbaro", 120, 90, 30, 95, 80);
+        _cards[6] = new Carta("A7", "Druida", 85, 75, 70, 65, 80);
+        _cards[7] = new Carta("A8", "Assassino", 75, 85, 95, 55, 60);
+        _cards[8] = new Carta("B1", "Necromante", 80, 90, 50, 70, 65);
+        _cards[9] = new Carta("B2", "Monge", 90, 80, 85, 60, 75);
+        _cards[10] = new Carta("B3", "Feiticeiro", 85, 90, 60, 70, 65);
+        _cards[11] = new Carta("B4", "Anão", 100, 85, 40, 80, 90);
+        _cards[12] = new Carta("B5", "Goblin", 60, 70, 50, 40, 30);
+        _cards[13] = new Carta("B6", "Orc", 110, 80, 30, 90, 70);
+        _cards[14] = new Carta("B7", "Troll", 120, 75, 20, 85, 80);
+        _cards[15] = new Carta("B8", "Dragão", 150, 100, 40, 95, 90);
+        _cards[16] = new Carta("C1", "Vampiro", 90, 85, 70, 80, 60);
+        _cards[17] = new Carta("C2", "Zumbi", 80, 60, 30, 50, 40);
+        _cards[18] = new Carta("C3", "Esqueleto", 70, 65, 40, 45, 35);
+        _cards[19] = new Carta("C4", "Golem", 130, 75, 20, 85, 95);
+        _cards[20] = new Carta("C5", "Anjo", 100, 90, 60, 80, 70);
+        _cards[21] = new Carta("C6", "Demônio", 110, 85, 50, 90, 80);
+        _cards[22] = new Carta("C7", "Fada", 80, 70, 90, 60, 50);
+        _cards[23] = new Carta("C8", "Sereia", 85, 75, 80, 65, 70);
+        _cards[24] = new Carta("D1", "Centauro", 95, 80, 70, 75, 65);
+        _cards[25] = new Carta("D2", "Minotauro", 120, 85, 30, 90, 80);
+        _cards[26] = new Carta("D3", "Fênix", 110, 90, 60, 85, 75);
+        _cards[27] = new Carta("D4", "Grifo", 100, 80, 70, 75, 65);
+        _cards[28] = new Carta("D5", "Hidra", 130, 85, 40, 90, 80);
+        _cards[29] = new Carta("D6", "Quimera", 120, 80, 50, 85, 75);
+        _cards[30] = new Carta("D7", "Gigante", 90, 75, 80, 65, 60);
+        _cards[31] = new Carta("D8", "Fantasma", 95, 80, 70, 75, 65);
     }
-    private void createCard()
+
+    public Carta[] getCards()
     {
-        _cards.Add(new Card("A1", "Guerreiro", 100, 85, 40, 80, 90));
-        _cards.Add(new Card("A2", "Mago", 80, 90, 60, 70, 60));
-        _cards.Add(new Card("A3", "Arqueiro", 90, 75, 80, 65, 70));
-        _cards.Add(new Card("A4", "Ladino", 70, 80, 90, 60, 50));
-        _cards.Add(new Card("A5", "Paladino", 110, 80, 50, 85, 95));
-        _cards.Add(new Card("A6", "Bárbaro", 120, 90, 30, 95, 80));
-        _cards.Add(new Card("A7", "Druida", 85, 75, 70, 65, 80));
-        _cards.Add(new Card("A8", "Assassino", 75, 85, 95, 55, 60));
-        _cards.Add(new Card("B1", "Necromante", 80, 90, 50, 70, 65));
-        _cards.Add(new Card("B2", "Monge", 90, 80, 85, 60, 75));
-        _cards.Add(new Card("B3", "Feiticeiro", 85, 90, 60, 70, 65));
-        _cards.Add(new Card("B4", "Anão", 100, 85, 40, 80, 90));
-        _cards.Add(new Card("B5", "Golbin", 60, 70, 50, 40, 30));
-        _cards.Add(new Card("B6", "Orc", 110, 80, 30, 90, 70));
-        _cards.Add(new Card("B7", "Troll", 120, 75, 20, 85, 80));
-        _cards.Add(new Card("B8", "Dragão", 150, 100, 40, 95, 90));
-        _cards.Add(new Card("C1", "Vampiro", 90, 85, 70, 80, 60));
-        _cards.Add(new Card("C2", "Zumbi", 80, 60, 30, 50, 40));
-        _cards.Add(new Card("C3", "Esqueleto", 70, 65, 40, 45, 35));
-        _cards.Add(new Card("C4", "Golem", 130, 75, 20, 85, 95));
-        _cards.Add(new Card("C5", "Anjo", 100, 90, 60, 80, 70));
-        _cards.Add(new Card("C6", "Demônio", 110, 85, 50, 90, 80));
-        _cards.Add(new Card("C7", "Fada", 80, 70, 90, 60, 50));
-        _cards.Add(new Card("C8", "Sereia", 85, 75, 80, 65, 70));
-        _cards.Add(new Card("D1", "Centauro", 95, 80, 70, 75, 65));
-        _cards.Add(new Card("D2", "Minotauro", 120, 85, 30, 90, 80));
-        _cards.Add(new Card("D3", "Fênix", 110, 90, 60, 85, 75));
-        _cards.Add(new Card("D4", "Grifo", 100, 80, 70, 75, 65));
-        _cards.Add(new Card("D5", "Hidra", 130, 85, 40, 90, 80));
-        _cards.Add(new Card("D6", "Quimera", 120, 80, 50, 85, 75));
-        _cards.Add(new Card("D7", "Gigante", 90, 75, 80, 65, 60));
-        _cards.Add(new Card("D8", "Fantasma", 95, 80, 70, 75, 65));
-    }
-    public List<Card> GetCards()
-    {
+        // função para retornar o array de cartas que tem no baralho
         return _cards;
     }
 }
-#endregion
 
-#region Julia
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bem-vindo ao jogo Super Trunfo - Fantasia Medieval!\nDigite seu nome:");
+        Console.WriteLine("Bem-vindo ao Super Trunfo - Fantasia Medieval!\nDigite seu nome: ");
         string nome = Console.ReadLine();
-        Console.WriteLine($"\nOlá, {nome}!:)\n---Menu de opções---\nDigite:\n1. Novo jogo\n2. Configurações\n3. Sair do jogo\n--------------------\n");
-
         bool sair = false;
         while (!sair)
         {
-            string entrada = Console.ReadLine();
-            int opcao;
-            int i;
-
-            if (int.TryParse(entrada, out opcao))
+            // loop para exibição do menu principal e suas opções
+            Console.WriteLine($"\nOlá, {nome}!\n----- Menu -----\n1. Novo Jogo\n2. Configurações\n3. Sair\n----------------");
+            string opcao = Console.ReadLine();
+            switch (opcao)
             {
-                if (opcao == 1)
-                {
-                    Console.WriteLine("\nIniciando um novo jogo...");
-                    Console.WriteLine("As cartas foram sorteadas e dividas\n");
-
-                    Deck baralho = new Deck();
-                    List<Card> cartas = new List<Card>(baralho.GetCards());
-
-                    List<Card> jogador = new List<Card>();
-                    List<Card> computador = new List<Card>();
-
-                    Random embaralho = new Random();
-
-                    for (i = 0; i < 15; i++)
-                    {
-                        int a = embaralho.Next(cartas.Count);
-                        int b = embaralho.Next(cartas.Count);
-
-                        Card temporaria = cartas[a];
-                        cartas[a] = cartas[b];
-                        cartas[b] = temporaria;
-                    }
-
-                    for (i = 0; i < cartas.Count; i++)
-                        if (i % 2 == 0)
-                        {
-                            jogador.Add(cartas[i]);
-                        }
-                        else
-                        {
-                            computador.Add(cartas[i]);
-                        }
-
-                    int rodada = 1;
-
-                    Random sorteada = new Random();
-                    int c = sorteada.Next(jogador.Count);
-                    jogador[c].displayCard();
-                    Console.WriteLine($"Essa é sua {rodada}º carta sorteada!\n");
-
-                    rodada++;
-
-                    if (rodada % 2 == 0)
-                    {
-                        Console.WriteLine("Escolha qual característica sua carta vai disputar nessa rodada:\n A. HP\n B. Poder\n C. Velocidade\n D. Força\n E. Resistência");
-                    }
-                    else
-                    {
-                        Random atributo_aleatorio = new Random();
-                    }
-                    string resposta = Console.ReadLine();
-
-                    if (resposta.ToUpper() == "A")
-                    {
-                        Console.WriteLine("Você escolheu disputar por HP");
-                    }
-                    else if (resposta.ToUpper() == "B")
-                    {
-                        Console.WriteLine("Você escolheu disputar por Poder!");
-                    }
-                    else if (resposta.ToUpper() == "C")
-                    {
-                        Console.WriteLine("Você escolheu disputar por Velocidade!");
-                    }
-                    else if (resposta.ToUpper() == "D")
-                    {
-                        Console.WriteLine("Você escolheu disputar por Força!");
-                    }
-                    else if (resposta.ToUpper() == "E")
-                    {
-                        Console.WriteLine("Você escolheu disputar por Resistência!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Opção inválida! Tente novamente.");
-                    }
-                }
-                else if (opcao == 2)
-                {
-                    Console.WriteLine("Abrindo configurações...");
-                    Console.WriteLine($"Olá, {nome}!\n" + "Nessa versão de fantasia medieval do jogo Super Trunfo, dois jogadores, sendo um o computador, recebem 16 cartas (31 cartas temáticas + 1 carta Super Trunfo), totalizando 32 cartas.");
-                }
-                else if (opcao == 3)
-                {
-                    Console.WriteLine("Saindo do jogo...");
+                case "1":
+                    iniciarJogo();
+                    break;
+                case "2":
+                    // limpa a tela e mostra as regras do jogo
+                    Console.Clear();
+                    Console.WriteLine("\nConfigurações:\nRegras:\nO baralho do jogo possui 32 cartas, sendo divido em duas pilhas de 16 para cada jogador.\nCada carta possui 5 atributos, sendo eles: HP, Poder, Velocidade, Força e Resistência.\nUm jogador receberá uma carta e deve ser escolhido um atributo. Se o seu atributo for maior que o atributo do outro jogador, você ganha a carta dele.\nNesta versão do jogo há apenas dois jogadores, sendo eles: você e o computador.");
+                    break;
+                case "3":
+                    // faz com que o programa saia do loop, terminando a execução do programa
                     sair = true;
-                }
-                else
-                {
-                    Console.WriteLine("Você digitou um valor inválido!");
-                }
+                    break;
+                default:
+                    // tratamento de erro para opções inválidas que o usuário inserir
+                    Console.WriteLine("Opção inválida. Por favor digite um valor válido.");
+                    break;
             }
         }
+    }
+    static void iniciarJogo()
+    {
+        // função que inicia o jogo, embaralhando as cartas, as dividindo entre os jogadores e comparando os atributos para determinar o vencedor do jogo
+        Random embaralhar = new Random(); // comando para selecionar um número aleatório
 
+        // cria um novo baralho
+        Baralho baralho = new Baralho();
+        Carta[] cartasTotal = baralho.getCards();
+
+        for (int i = cartasTotal.Length - 1; i > 0; i--)
+        {
+            // embaralha as cartas
+            int j = embaralhar.Next(i + 1);
+            Carta carta = cartasTotal[i];
+            cartasTotal[i] = cartasTotal[j];
+            cartasTotal[j] = carta;
+        }
+
+        // cria dois baralhos, um para o jogador e outro para o computador
+        Carta[] jogadorBaralho = new Carta[32]; // tamanho maior para caber as cartas que ganhar
+        Carta[] computadorBaralho = new Carta[32];
+
+        int totalJogador = 16;
+        int totalComputador = 16;
+
+        for (int i = 0; i < 16; i++)
+        {
+            // distribui as cartas para os jogadores
+            jogadorBaralho[i] = cartasTotal[i];
+            computadorBaralho[i] = cartasTotal[i + 16];
+        }
+
+        while (totalJogador > 0 && totalComputador > 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"\n--- Placar: Você : {totalJogador} | Computador : {totalComputador} ---");
+
+            // atribui a primeira carta do baralho de cada jogador a uma variável para comparação
+            Carta jCarta = jogadorBaralho[0];
+            Carta cCarta = computadorBaralho[0];
+
+            bool escolhaValida = false; // variável para controle de validez da respostas do usuário
+
+            int aJogador = 0, aComputador = 0; // variáveis para guardar o valor do atributo selecionado
+
+            while (!escolhaValida) // loop para tratar erro de resposta inválida
+            {
+                jCarta.displayCard();
+                Console.WriteLine("Escolha o atributo:\nA. HP\nB. Poder\nC. Velocidade\nD. Força\nE. Resistência\n");
+                string escolha = Console.ReadLine().ToUpper();
+
+                escolhaValida = true; // assume que a resposta é válida, a não ser que caia no default
+
+                switch (escolha)
+                {
+                    case "A":
+                        aJogador = jCarta.Hp;
+                        aComputador = cCarta.Hp;
+                        break;
+                    case "B":
+                        aJogador = jCarta.Power;
+                        aComputador = cCarta.Power;
+                        break;
+                    case "C":
+                        aJogador = jCarta.Speed;
+                        aComputador = cCarta.Speed;
+                        break;
+                    case "D":
+                        aJogador = jCarta.Strength;
+                        aComputador = cCarta.Strength;
+                        break;
+                    case "E":
+                        aJogador = jCarta.Resistance;
+                        aComputador = cCarta.Resistance;
+                        break;
+                    default:
+                        Console.WriteLine("Valor inválido! Por favor insira um valor válido.");
+                        escolhaValida = false; // repete o loop
+                        break;
+                }
+            }
+            
+            Console.WriteLine($"\nSua carta: {jCarta.Name} ({aJogador}) vs Carta do Computador: {cCarta.Name} ({aComputador})");
+
+            // diminui os contadores antes de mover as cartas para não dar erro e o programa tentar ultrapassar o limite do array
+            totalJogador--;
+            totalComputador--;
+
+            for (int i = 0; i < totalJogador; i++)
+            {
+                //puxa todas as cartas do baralho do usuário para frente
+                jogadorBaralho[i] = jogadorBaralho[i + 1];
+            }
+
+            for (int i =0; i < totalComputador; i++)
+            {
+                //puxa todas as cartas do baralho do computador para frente
+                computadorBaralho[i] = computadorBaralho[i + 1];
+            }
+
+            if (aJogador > aComputador)
+            {
+                Console.WriteLine("Você VENCEU a rodada! +1 carta.\n");
+                jogadorBaralho[totalJogador] = jCarta; // adiciona a carta do usuário no final do array do baralho do usuário
+                jogadorBaralho[totalJogador + 1] = cCarta; // adiciona a carta do computador no final do array do baralho do usuário
+                totalJogador += 2; // aumenta dois de espaço para as cartas do baralho do usuário
+            }
+            else if (aComputador > aJogador)
+            {
+                // adiciona a carta do usuário e do computador ao baralho do computador
+                Console.WriteLine("Você PERDEU a rodada! O computador pegou sua carta.\n");
+                computadorBaralho[totalComputador] = cCarta; // adiciona a carta do computador no final do array do baralho do computador
+                computadorBaralho[totalComputador + 1] = jCarta; // adiciona a carta do usuário no final do array do baralho do computador
+                totalComputador += 2; // aumenta dois de espaço para as cartas do baralho do computador
+            }
+            else
+            {
+                // adiciona a carta do usuário ao baralho do usuário e a carta do computador ao baralho do computador
+                Console.WriteLine("EMPATE! Cada um fica com sua carta.");
+                jogadorBaralho[totalJogador] = jCarta; // adiciona a carta do usuário no final do array do baralho do próprio usuário
+                computadorBaralho[totalComputador] = cCarta; // adiciona a carta do computador no final do array do baralho do própri computador
+                totalJogador++;
+                totalComputador++;
+            }
+
+            if (totalJogador >=32 || totalComputador >= 32)
+            {
+                // se qualquer um dos dois arrays chegar no 32 ou tentar ultrapassá-lo, significa que o baralho conquistou todas as cartas do jogo, decidindo o vencedor
+                break;
+            }
+
+            Console.WriteLine("Pressione Enter para próxima rodada...");
+            Console.ReadLine();
+        }
+
+        if (totalJogador > 0)
+        {
+            // se apenas a quantidade de cartas do baralho do usuário for maior que 0, significa que a do computador é 0, então o computador perde
+            Console.WriteLine("\nPARABÉNS! Você ganhou o jogo!");
+        }
+        else
+        {
+            // se apenas a quantidade de cartas do baralho do computador for maior que 0, significa que a do usuário é 0, então o usuário perde
+            Console.WriteLine("\nQue pena... O computador venceu.");
+        }
     }
 }
-#endregion
